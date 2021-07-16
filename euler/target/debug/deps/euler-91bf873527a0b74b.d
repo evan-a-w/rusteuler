3179a8720b19afa85e5267b67203d742851c8271
Evan Williams <evanawilliams@live.com>
2021-07-16 14:12:40 +1000

    fix

---
+++ euler/target/debug/deps/euler-91bf873527a0b74b.d
@@ -0,0 +1,6 @@
+/home/evan/dev/rusteuler/euler/target/debug/deps/euler-91bf873527a0b74b: src/main.rs src/probs.rs
+
+/home/evan/dev/rusteuler/euler/target/debug/deps/euler-91bf873527a0b74b.d: src/main.rs src/probs.rs
+
+src/main.rs:
+src/probs.rs:
---
+++ euler/target/debug/euler.d
@@ -0,0 +1,1 @@
+/home/evan/dev/rusteuler/euler/target/debug/euler: /home/evan/dev/rusteuler/euler/src/main.rs /home/evan/dev/rusteuler/euler/src/probs.rs
