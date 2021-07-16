6d1c9ff911236d0b006952d5b17b1685c55fe9d7
Evan Williams <evanawilliams@live.com>
2021-07-16 13:21:56 +1000

    p1

---
+++ euler/src/main.rs
@@ -0,0 +1,6 @@
+mod probs;
+
+fn main() 
+{
+    println!("{}", probs::prob2());
+}
---
+++ euler/src/probs.rs
@@ -0,0 +1,69 @@
+use math::round;
+
+pub fn prob1() -> usize
+{
+    (3..1000).filter(|x| x % 3 == 0 || x % 5 == 0).sum()
+}
+
+pub fn fibs(n : usize) -> Vec<usize>
+{
+    if n < 3
+    {
+        return vec![1; n];
+    }
+    let mut res : Vec<usize> = vec![1; n];
+    for i in 2..n
+    {
+        res[i] = res[i-1] + res[i-2];
+    }
+    
+    res
+}
+
+pub fn fibs_while_less(n : usize) -> Vec<usize>
+{
+    let mut res : Vec<usize> = vec![1, 1];
+    let mut cur : usize = res[0] + res[1];
+    
+    let mut i : usize = 3;
+    while cur < n
+    {
+        res.push(cur);
+        cur = res[i-1] + res[i-2];
+        i += 1;
+    }
+
+    res
+}
+
+pub fn prob2() -> usize
+{
+    let fib = fibs_while_less(4000000);
+    fib.into_iter().filter(|x| x % 2 == 0).sum()
+}
+
+pub fn prime_factors(n : usize) -> Vec<usize>
+{
+    let mut res : Vec<usize> = Vec::new();
+    let go = ||
+    {
+        let mut ex : bool = false;
+        for i in 1..((n as f64).sqrt().ceil() as usize)
+        {
+            if n % i == 0
+            {
+                res.push(i);
+                n /= i;
+                ex = true;
+            }
+            if ex
+            {
+                break;
+            }
+        }
+        if ex
+        {
+
+        }
+    }
+}
